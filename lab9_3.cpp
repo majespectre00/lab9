d0c12f9b913899f12d4f65e430c81402bbfd793f
majespectre00 <oat601_12@hotmail.com>
2021-01-08 16:46:23 +0700

    failed some hidden case

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main (){
+    double sum=0,sumsquare = 0;
+    int N=0;
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sumsquare += pow(atof(textline.c_str()),2);
+        N++;}
+    double mean,sd;
+    mean = sum/N;
+    sd =sqrt((sumsquare/N)-pow(mean,2));
+    cout<<fixed<<setprecision(3);
+    cout << "Number of data = "<<N<<endl;
+    cout << "Mean = "<<mean<<endl;
+    cout << "Standard deviation = "<<sd;
+}
